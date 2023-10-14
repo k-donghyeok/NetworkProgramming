83947bd263a91df4153b9baaaea7e27aeadc08d0
k-donghyeok <rkdehdgur432@naver.com>
2023-10-14 21:52:22 +0900

    전송 손실을 보완한 udp 프로그램

--- udp_send_receive/udp_client_study.py
+++ udp_send_receive/udp_client_study.py
@@ -5,5 +5,20 @@ port=1234
 address = ('',port)
 
 data="udp client 데이터입니다 "
-udp_client_socket.sendto(data.encode(),('localhost',port))
+
+for i in range(10):
+    delay = 0.1
+    while True:
+        udp_client_socket.sendto(data.encode(), ('localhost', port))
+        print('{} 시간동안 기다리겠습니다'.format(delay))
+        udp_client_socket.settimeout(delay)
+        try:
+            recvdata, server_address = udp_client_socket.recvfrom(1024)
+        except socket.timeout:
+            delay *=2
+            if delay >2.0:
+                break
+        else:
+            print('클라이언트에서 수신한 데이터:',recvdata.decode())
+            break
 
--- udp_send_receive/udp_server_study.py
+++ udp_send_receive/udp_server_study.py
@@ -1,12 +1,22 @@
 import socket
-
+import random
 udp_server_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 if(udp_server_socket.bind(('',1234))==-1):
     print("서버 할당 실패")
 
-data,client_address =udp_server_socket.recvfrom(1024)
 
-print(data.decode())
+
+str = "ACK"
+while True:
+    data, client_address = udp_server_socket.recvfrom(1024)
+    if ((random.randint(1, 10) < 5)):
+        print("송신실패")
+        continue
+    else:
+        print(data.decode())
+        udp_server_socket.sendto(str.encode(), client_address)
+
+
 
 
